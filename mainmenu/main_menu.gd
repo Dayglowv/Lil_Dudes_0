4f86ceb82827b4654014321c89f4d1588815698d
EthanGmoney <ethangonzales1407@gmail.com>
2024-05-19 12:25:49 -0700

    Start Button now works

--- mainmenu/main_menu.gd
+++ mainmenu/main_menu.gd
@@ -4,7 +4,7 @@ extends Control
 
 @onready var start_button = $MarginContainer/HBoxContainer/VBoxContainer/Start_button as Button
 @onready var exit_button = $MarginContainer/HBoxContainer/VBoxContainer/Exit_Button as Button
-@onready var start_level = preload("res://mainmenu/main_menu.tscn")
+@onready var start_level = preload("res://Level1.tscn") as PackedScene
 
 
 func _ready():
@@ -12,7 +12,7 @@ func _ready():
 	exit_button.button_down.connect(on_exit_pressed)
 
 func on_start_pressed() -> void:
-	pass
+	get_tree().change_scene_to_packed(start_level)
 
 
 func on_exit_pressed() -> void:
