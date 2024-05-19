51574a6801849cf2ae152d4a830eeee3d8a4fe4a
AidanLau10 <aidanlau10@gmail.com>
2024-05-19 11:23:53 -0700

    movement

---
+++ player.gd
@@ -0,0 +1,25 @@
+extends CharacterBody2D
+
+@export var SPEED : int = 150
+@export var JUMP_FORCE : int = 255
+@export var GRAVITY : int = 900
+func _physics_process(delta):
+	
+	var direction = Input.get_axis("Left", "Right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = 0
+	
+	if direction == 1:
+		$AnimatedSprite2D.flip_h = false
+	elif direction == -1:
+		$AnimatedSprite2D.flip_h = true
+	if not is_on_floor():
+
+		velocity.y += GRAVITY * delta
+	
+	if Input.is_action_just_pressed("Jump") and is_on_floor():
+		velocity.y -= JUMP_FORCE
+		$AnimatedSprite2D.play("Jump")
+		
